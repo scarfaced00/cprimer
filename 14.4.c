15e252a88f691094d11eb12bf48d1b1d22e0037c
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2021-06-18 11:30:42 +0900

    14.5

--- 14.4.c
+++ 14.4.c
@@ -1,41 +1,31 @@
-/*friends.c*/
+/*funds1.c*/
 #include <stdio.h>
-#define LEN 20
+#define FUNDLEN 50
 
-struct names {
-	char first[LEN];
-	char last[LEN];
+struct funds {
+	char bank[FUNDLEN];
+	double bankfund;
+	char save[FUNDLEN];
+	double savefund;
 };
 
-struct guy{
-	struct names handle;
-	char favfood[LEN];
-	char job[LEN];
-	float income;
-};
+double sum(double, double);
 
 int main(void)
 {
-	struct guy fellow[2] = {
-		{{"Ewen", "Villard"},
-		"grilled salmon",
-		"personality coach",
-		68112.00
-		},
-		{{"Rodney", "Swillbelly"},
-		"tripe",
-		"tabloid editor",
-		232400.00
-		}
+	struct funds stan = {
+		"Garlic-Melon Bank",
+		4032.27,
+		"Lucky's Savings and Loan",
+		8543.94
 	};
-	struct guy *him; /*here is a pointer to a structure*/
 	
-	printf("address #1: %p #2: %p\n", &fellow[0], &fellow[1]);
-	him = &fellow[0]; /*tell the pointer where to point*/
-	printf("pointer #1: %p #2: %p\n, him, him+1");
-	printf("him->income is $%.2f: (*him).income is $%.2f\n", him->income, (*him).income);
-	him++; /*point to the next structure*/
-	printf("him->favfood is %s: him->handle.last is %s\n", him->favfood, him->handle.last);
-	
+	printf("Stan has a total of $%.2f.\n", sum(stan.bankfund, stan.savefund));
 	return 0;
+}
+
+/* adds two double numbers*/
+double sum(double x, double y)
+{
+	return (x+y);
 }
