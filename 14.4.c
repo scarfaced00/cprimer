3a3c314f0cf2aaa60026e55a86561707e78adba0
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2021-06-18 12:19:37 +0900

    14.9

--- 14.4.c
+++ 14.4.c
@@ -1,4 +1,4 @@
-/*names1.c -- ueses pointers to a structure*/
+/*names2.c -- passes and returns structures*/
 #include <stdio.h>
 #include <string.h>
 #define NLEN 30
@@ -9,36 +9,44 @@ struct namect {
 	int letters;
 };
 
-void getinfo(struct namect *);
-void makeinfo(struct namect *);
-void showinfo(const struct namect *);
+struct namect getinfo(void);
+struct namect makeinfo(struct namect);
+void showinfo(struct namect);
 char * s_gets(char * st, int n);
 
 int main(void)
 {
 	struct namect person;
 	
-	getinfo(&person);
-	makeinfo(&person);
-	showinfo(&person);
+	person = getinfo();
+	person = makeinfo(person);
+	showinfo(person);
+
 	return 0;
 }
 
 
-void getinfo(struct namect * pst)
+struct namect getinfo(void)
 {
+	struct namect temp;
 	printf("Please enter your first name.\n");
-	s_gets(pst->fname, NLEN);
+	s_gets(temp.fname, NLEN);
 	printf("Please enter your last name.\n");
-	s_gets(pst->lname, NLEN);
+	s_gets(temp.lname, NLEN);
+	
+	return temp;
 }
-void makeinfo(struct namect * pst)
+
+struct namect makeinfo(struct namect info)
 {
-	pst->letters = strlen(pst->fname)+strlen(pst->lname);
+	info.letters = strlen(info.fname) + strlen(info.lname);
+	
+	return info;
 }
-void showinfo(const struct namect * pst)
+
+void showinfo(struct namect info)
 {
-	printf("%s %s, your name contains %d letters.\n", pst->fname, pst->lname, pst->letters);
+	printf("%s %s, your name contains %d letters.\n", info.fname, info.lname, info.letters);
 }
 
 char * s_gets(char *st, int n)
