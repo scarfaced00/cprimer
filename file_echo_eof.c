52720b5dc4abb2e2e8544c09baecaa3c14abd6ae
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2021-06-20 13:19:00 +0900

    good_design#1

--- file_echo_eof.c
+++ file_echo_eof.c
@@ -1,28 +1,17 @@
-/*count.c--using standard i/o*/
+//good design #1
 #include <stdio.h>
-#include <stdlib.h> //exit() prototype
+#include <stdlib.h> 
 int main(int argc, char *argv[])
 {
-	int ch; /*place to store each character as read*/
-	FILE *fp; //"file pointer"
-	unsigned long count = 0;
-	if (argc != 2)
+	int ch; //int to hold EOF
+	FILE *fp; 
+	fp = fopen("wacky.txt", "r"); 
+	ch = getc(fp); //get initial input
+	while(ch != EOF)
 	{
-		printf("Usage: %s filename\n", argv[0]);
-		exit(EXIT_FAILURE);
+		putchar(ch); //process input
+		ch = getc(fp); //get next input
 	}
-	if ((fp = fopen(argv[1], "r")) == NULL)
-	{
-		printf("Can't open %s\n', argv[1]");
-		exit(EXIT_FAILURE);
-	}
-	while ((ch = getc(fp)) != EOF)
-	{
-		putc(ch, stdout); //same as putchar(ch);
-		count++;
-	}
-	fclose(fp);
-	printf("File %s has %lu characters\n", argv[1], count);
 	
 	return 0;
 }
