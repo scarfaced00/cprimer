e7433cfa65909436622b39930c77dca235ba003d
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2021-06-21 18:32:02 +0900

    8.5

--- file_echo_eof.c
+++ file_echo_eof.c
@@ -1,33 +1,32 @@
-/*reverse.c--displays a file in reverse order*/
+/*showchar1.c--program with a BIG I/O problem*/
 #include <stdio.h>
-#include <stdlib.h>
-#define CNTL_Z '\032' /*eof marker in DOS text files*/
-#define SLEN 81
+void display(char cr, int lines, int width);
+
 int main(void)
 {
-	char file[SLEN]; 
-	char ch;
-	FILE *fp;
-	long count, last;
-	
-	puts("Enter the name of the file to be processed:");
-	scanf("%80s", file);
-	if ((fp = fopen(file, "rb")) == NULL)
+	int ch; //character to be printed
+	int rows, cols; //number of rows and columns
+	printf("Enter a character and two integers:\n");
+	while ((ch = getchar()) != '\n')
 	{
-		printf("reverse can't open %s\n", file);
-		exit(EXIT_FAILURE);
+		scanf("%d %d", &rows, &cols);
+		display(ch, rows, cols);
+		printf("Enter another character and two integers;\n");
+		printf("Enter a newline to quit.\n");
 	}
-	fseek(fp, 0L, SEEK_END) /*go to end of file*/
-	last = ftell(fp);
-	for (count = 1L, count <= last; count++)
-	{
-		fseek(fp, -count, SEEK_END); /*go backward*/
-		ch = getc(fp);
-		if (ch != CNTL_Z && ch != '\r') /*MS-DOS files*/
-			putchar(ch);
-	}
-	putchar('\n');
-	fclose(fp);
+	printf("Bye.\n");
 	
 	return 0;
+}
+
+void display(char cr, int lines, int width)
+{
+	int row, col;
+	
+	for (row = 1; row <= lines; row++)
+	{
+		for (col = 1; col <= width; col++)
+			putchar(cr);
+		putchar('\n'); //end line andn start a new one
+	}
 }
